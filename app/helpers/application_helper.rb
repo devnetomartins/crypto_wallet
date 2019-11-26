156a2e5265eb6b3994c37c365174dad2f914d341
Agostinho Neto <agostinhomartinsneto@gmail.com>
2019-11-26 11:29:28 -0300

    Update project

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,16 @@
 module ApplicationHelper
     def date_br
-
         Date.today.strftime("%d/%m/%Y")
+    end
+
+    def env_rails
+        if Rails.env.development?
+            "Desenvolvimento"
+        elsif Raisl.env.production?
+            "Produção"
+        else
+            "Testes"
+        end
     end
     
 end
