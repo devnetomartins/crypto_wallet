ba59d9e6958e1f809f14412dc1e983104dc89e18
Agostinho Neto <agostinhomartinsneto@gmail.com>
2020-01-18 18:11:59 -0300

    Criado seeds para inclusao no banco para testes

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Coin.create!(
+    description: "Bitcoin",
+    acronym: "BTC",
+    url_image: "http://pngimg.com/uploads/bitcoin/bitcoin_PNG16.png"
+)
+
+Coin.create!(
+    description: "Ethereum",
+    acronym: "ETH",
+    url_image: "https://upload.wikimedia.org/wikipedia/commons/b/b7/ETHEREUM-YOUTUBE-PROFILE-PIC.png"
+)
