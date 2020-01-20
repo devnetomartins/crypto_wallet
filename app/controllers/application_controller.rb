479804e92708b84d094f1889172f793f4a5e5c9f
Agostinho Neto <agostinhomartinsneto@gmail.com>
2020-01-20 16:53:47 -0300

    link para mudança de idioma na pagina home

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,16 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  before_action :set_locale
+
+  def set_locale
+    if params[:locale]
+      cookies[:locale] = params[:locale]
+    end
+
+    if cookies[:locale]
+      if I18n.locale != cookies[:locale]
+        I18n.locale = cookies[:locale]
+      end
+    end
+  end
 end
